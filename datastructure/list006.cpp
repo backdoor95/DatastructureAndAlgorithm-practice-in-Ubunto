930ad0c42db551da0291093d5d0fab3b15549f4b
MunJunHo <mjh1995713@gmail.com>
2023-12-24 10:56:37 +0900

    initGroup

---
+++ datastructure/list006.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 설계 방안 A의 구현 2 : 연결리스트 이용
+typedef struct Node
+{
+    char elem;
+    char group;
+    Node *prev;
+    Node *next;
+} Node;
+typedef struct List
+{
+    Node *H;
+    Node *T;
+    int n;
+} List;
+Node *getNode()
+{
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->elem = '0';
+    p->group = '0';
+    p->next = NULL;
+    p->prev = NULL;
+    return p;
+}
+List *initGroup()
+{
+    List *list = (List *)malloc(sizeof(List));
+    list->H = getNode();
+    list->T = getNode();
+    list->n = 1000;
+    return list;
+}
+List *initGroup2()
+{
+    Node *H = getNode();
+    Node *T = getNode();
+    H->next = T;
+    T->prev = H;
+
+    List *list = (List *)malloc(sizeof(List)); // 메모리 할당
+    list->H = H;
+    list->T = T;
+    list->n = 2000;
+    return list;
+}
+int main()
+{
+    List *list1 = initGroup();
+
+    List *list2 = initGroup2();
+
+    printf("list1 test = %d\n", list1->n);
+    printf("list1 H = %p | list1 T = %p\n", list1->H, list1->T);
+    printf("list1 H.next = %p | list1 T.prev = %p\n", list1->H->next, list1->T->prev);
+    printf("list2 test = %d\n", list2->n);
+    printf("list2 H = %p | list2 T = %p\n", list2->H, list2->T);
+    printf("list2 H.next = %p | list2 T.prev = %p\n", list2->H->next, list2->T->prev);
+    return 0;
+}
