d3b78012b0bc28026e768fa0bba51a2c9c3c7b70
MunJunHo <mjh1995713@gmail.com>
2024-01-02 01:02:11 +0900

    설계 B : 연결리스트의 배열 이용 -삽입, 삭제

--- datastructure/list008.cpp
+++ datastructure/list008.cpp
@@ -3,14 +3,15 @@
 // 설계 B 구현 2:연결리스트의 배열 이용
 typedef struct Node
 {
-    Node *prev;
-    Node *next;
+    struct Node *prev;
+    struct Node *next;
     char elem;
 } node;
 typedef struct Group
 {
-    node *H;
-    node *T;
+    node **H;
+    node **T;
+    int M;
 } group;
 node *getnode()
 {
@@ -18,15 +19,50 @@ node *getnode()
     return newNode;
 }
 
-group** initGroup(int M)
+group *initGroup(int M)
 {
-    group** ppg;
-    
+    group *g = (group *)malloc(sizeof(group));
 
-    return ppg;
+    g->H = (node **)malloc(M * sizeof(group *));
+    g->T = (node **)malloc(M * sizeof(group *));
+    g->M = M;
+
+    // -> = (*).
+
+    for (int i = 0; i < M; i++)
+    {
+        node *h = getnode();
+        node *t = getnode();
+        h->next = t;
+        t->prev = h;
+        g->H[i] = h;
+        g->T[i] = t;
+    }
+
+    return g;
 }
+// 순회
+void traverseGroupu(group* g, int gIdx)
+{
+    node *p = g->H[gIdx];
+    while (p != g->T[gIdx])
+    {
+        printf("%d\n", p->elem);
+        p= p->next;
+    }
+    return;
+}
+
+// 삽입
+void addGroupFirst(int g, char e)
+{
+
+}
+
 int main()
 {
+    int M = 10;
+    group *pg = initGroup(M);
 
     return 0;
 }
