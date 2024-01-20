abd77637f6d2b29fb285a326430fee02747c9cce
MunJunHo <mjh1995713@gmail.com>
2024-01-20 17:13:33 +0900

    feat: 범위기반 for문

---
+++ algorithm/memset.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main()
+{
+    cin >> n;
+    int a[n];
+
+
+    memset(a, 0, sizeof(a));
+    for (int i : a)
+    {
+        cout << i << " ";
+    }
+    // cout << endl;
+    // cout << "-----------------------" << endl;
+    // memset(c, 0, sizeof(c));
+    // for (int i : c)
+    // {
+    //     cout << i << " ";
+    // }
+
+    return 0;
+}
