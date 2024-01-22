b11881c022fa136d0f1d9aa139aecef374f5e06f
MunJunHo <mjh1995713@gmail.com>
2024-01-22 14:58:06 +0900

    feat: int 나누기

---
+++ algorithm/int.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a = 3, b = 2;
+    cout << a / b << endl;
+    double c = 3, d = 2;
+    cout << c / d << endl;
+}
