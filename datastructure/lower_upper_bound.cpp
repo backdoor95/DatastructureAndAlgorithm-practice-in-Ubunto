b1b0381bd6b0e003216e94c6a5a839d147b32e26
MunJunHo <mjh1995713@gmail.com>
2024-01-25 13:45:53 +0900

    feat:upper_bound  lower_bound

---
+++ datastructure/lower_upper_bound.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> a{1, 2, 3, 3, 3, 4};
+    cout << lower_bound(a.begin(), a.end(), 3) - a.begin() << "\n";
+    cout << upper_bound(a.begin(), a.end(), 3) - a.begin() << "\n";
+
+    return 0;
+}
