ac4b7b867a225dd311acbb74c1ed6b53706b935c
MunJunHo <mjh1995713@gmail.com>
2024-02-07 21:25:28 +0900

    feat: 2309-일곱난장이 sol_2) 순열

---
+++ algorithm/first/1A2309-2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[9];
+
+void printV(vector<int> v)
+{
+    for (int i : v)
+        cout << i << "\n";
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 일곱 난장이
+
+    // 3. 순열을 이용해서 풀 수 있다. 9P7
+
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + 9);
+    vector<int> v;
+
+    do
+    {
+        int sum = 0;
+        for(int i = 0; i<7; i++) sum += a[i];
+        if(sum == 100) break;
+    } while (next_permutation(a, a+9));
+    
+    for(int i=0;i<7;i++)
+    cout<<a[i]<<"\n";
+    return 0;
+}
